cec0b220663b10cd031752759e17faa24477ea47
Agung Gumelar <gunghello@gmail.com>
2025-05-06 15:22:48 +0700

    feat(supplier): Add feature insert, update, delete, read, and search

--- src/dao/unitDAO.java
+++ src/dao/unitDAO.java
@@ -10,30 +10,32 @@ import model.Unit;
 
 /**
  *
- * @author Agung
+ * @author asus
  */
-
 public class UnitDAO {
     private Connection connection;
 
     public UnitDAO() {
         this.connection = Database.getConnection();
     }
-    
+
     public List<Unit> findAll() {
         List<Unit> list = new ArrayList<>();
 
-        String sql = "SELECT * FROM units";
+        String sql = "SELECT * FROM units i";
+
 
         try {
             PreparedStatement stmt = this.connection.prepareStatement(sql);
-            ResultSet rs = stmt.executeQuery(sql);
+            ResultSet rs = stmt.executeQuery();
 
             while (rs.next()) {
-                list.add(new Unit(
+                Unit item = new Unit(
                         rs.getInt("id"),
                         rs.getString("name")
-                ));
+                );
+
+                list.add(item);
             }
 
         } catch (Exception e) {
