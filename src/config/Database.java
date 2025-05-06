51e6885245ef15a28f6615351ce0bed96db5a6c0
Agung Gumelar <gunghello@gmail.com>
2025-05-06 17:17:54 +0700

    add: Config .env and database

--- src/config/Database.java
+++ src/config/Database.java
@@ -4,16 +4,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 /**
  *
  * @author Agung
  */
 
+
 public class Database {
+    
+    private static final Dotenv dotenv = Dotenv.load();
     private static final String DB_Database = "wms";
     private static final String DB_URL = "jdbc:mysql://mysql-8cf3a2-wms-project.i.aivencloud.com:11912/" + DB_Database;
     private static final String DB_USER = "avnadmin";
-    private static final String DB_PASSWORD = "AVNS_0J0kZ-OJAgDO7IMTK9n";
+    private static final String DB_PASSWORD = dotenv.get("DB_PASSWORD");
     
     private static Connection connection;
     
